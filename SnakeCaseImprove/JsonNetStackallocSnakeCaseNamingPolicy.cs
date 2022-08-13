42a51143c694c8c723b7cc8dead281f5d392b05c
Kacper Kondracki <kacper-cirno@protonmail.com>
2022-08-13 12:41:41 +0200

    Initial commit

---
+++ SnakeCaseImprove/JsonNetStackallocSnakeCaseNamingPolicy.cs
@@ -0,0 +1,83 @@
+﻿using System.Text.Json;
+
+namespace SnakeCaseImprove;
+
+public class JsonNetStackallocSnakeCaseNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name)
+    {
+        if (string.IsNullOrEmpty(name)) return name;
+
+        var index = 0;
+        var state = SeparatedCaseState.Start;
+
+        var width = name.Length + name.Length / 2;
+
+        Span<char> span = stackalloc char[width];
+
+        for (var i = 0; i < name.Length; i++)
+            if (name[i] == ' ')
+            {
+                if (state != SeparatedCaseState.Start) state = SeparatedCaseState.NewWord;
+            }
+            else if (char.IsUpper(name[i]))
+            {
+                switch (state)
+                {
+                    case SeparatedCaseState.Upper:
+                        var hasNext = i + 1 < name.Length;
+                        if (i > 0 && hasNext)
+                        {
+                            var nextChar = name[i + 1];
+                            if (!char.IsUpper(nextChar) && nextChar != '_')
+                            {
+                                span[index] = '_';
+                                index++;
+                            }
+                        }
+
+                        break;
+                    case SeparatedCaseState.Lower:
+                    case SeparatedCaseState.NewWord:
+                        span[index] = '_';
+                        index++;
+                        break;
+                }
+
+                var c = char.ToLowerInvariant(name[i]);
+
+                span[index] = c;
+                index++;
+
+                state = SeparatedCaseState.Upper;
+            }
+            else if (name[i] == '_')
+            {
+                span[index] = '_';
+                index++;
+                state = SeparatedCaseState.Start;
+            }
+            else
+            {
+                if (state == SeparatedCaseState.NewWord)
+                {
+                    span[index] = '_';
+                    index++;
+                }
+
+                span[index] = name[i];
+                index++;
+                state = SeparatedCaseState.Lower;
+            }
+
+        return new string(span[..index]);
+    }
+
+    private enum SeparatedCaseState
+    {
+        Start,
+        Lower,
+        Upper,
+        NewWord
+    }
+}
---
+++ SnakeCaseImprove/RemoraSnakeCaseNamingPolicy.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace SnakeCaseImprove;
+
+public class RemoraSnakeCaseNamingPolicy : JsonNamingPolicy
+{
+    private readonly bool _upperCase;
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="RemoraSnakeCaseNamingPolicy" /> class.
+    /// </summary>
+    /// <param name="upperCase">Whether the converted names should be in all upper case.</param>
+    public RemoraSnakeCaseNamingPolicy(bool upperCase = false)
+    {
+        _upperCase = upperCase;
+    }
+
+    /// <inheritdoc />
+    public override string ConvertName(string name)
+    {
+        if (string.IsNullOrEmpty(name)) return name;
+
+        var builder = new StringBuilder();
+
+        var wordBoundaries = new List<int>();
+
+        char? previous = null;
+        for (var index = 0; index < name.Length; index++)
+        {
+            var c = name[index];
+
+            if (previous.HasValue && char.IsUpper(previous.Value) && char.IsLower(c)) wordBoundaries.Add(index - 1);
+
+            if (previous.HasValue && char.IsLower(previous.Value) && char.IsUpper(c)) wordBoundaries.Add(index);
+
+            previous = c;
+        }
+
+        for (var index = 0; index < name.Length; index++)
+        {
+            var c = name[index];
+            if (wordBoundaries.Contains(index) && index != 0) builder.Append('_');
+
+            builder.Append(char.ToLowerInvariant(c));
+        }
+
+        return _upperCase
+            ? builder.ToString().ToUpperInvariant()
+            : builder.ToString();
+    }
+}
